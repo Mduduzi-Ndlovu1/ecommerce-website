19ddd2102eeee2a3bf220c8e0339493174eb5e30
Mduduzindlovu <Mduduzindlovu61@gmail.com>
2023-10-08 16:17:50 +0200

    home page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,31 @@ var swiperCategories = new Swiper(".catergories__container", {
   });
 /*=============== SWIPER PRODUCTS ===============*/
 
+var swiperProducts = new Swiper(".new__container", { 
+    spaceBetween:24,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1400: {
+          slidesPerView: 4,
+          spaceBetween: 24,
+        },
+    mousewheel: true,
+    keyboard: true,
+    }
+  });
 /*=============== PRODUCTS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
     tabContent = document.querySelectorAll('[content]');
